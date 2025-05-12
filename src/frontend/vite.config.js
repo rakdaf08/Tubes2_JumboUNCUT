7c993e5fda9539d2c5110f2a176e75d93286139d
Raka Daffa <raka.daffa2005@gmail.com>
2025-05-13 02:21:37 +0700

    fix: vite config

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port:3000,
     // Pengaturan dev server
     proxy: {
       // Proxy API requests untuk development lokal
